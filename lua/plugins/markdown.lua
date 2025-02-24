69b5f2c607c8e6dec78315cb44b72da8c37b7f07
Daniel Vieites <dvieitest@gmail.com>
2025-02-24 16:27:58 +0100

    add markview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,8 +1,12 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && yarn install",
-    cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
-    init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-    end,
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    config = function()
+        require("markview").setup({
+            preview = {
+                icon_provider = "mini"
+            }
+        })
+        require("markview.extras.checkboxes").setup({})
+    end
 }
