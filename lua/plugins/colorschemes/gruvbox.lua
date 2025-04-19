ed65ddeed5b3e6dcbbed6aac6c4801c3601e1a40
Daniel Vieites <dvieitest@gmail.com>
2025-04-19 11:38:26 +0200

    set gruvbox theme

--- lua/plugins/colorschemes/gruvbox.lua
+++ lua/plugins/colorschemes/gruvbox.lua
@@ -1,21 +1,8 @@
 return {
-    "ellisonleao/gruvbox.nvim",
+    "sainnhe/gruvbox-material",
     priority = 1000,
     lazy = false,
     config = function()
-        require("gruvbox").setup({
-            bold = false,
-            italic = {
-                strings = false,
-                emphasis = false,
-                comments = false,
-                operators = false,
-                folds = false,
-            },
-            inverse = true, -- invert background for search, diffs, statuslines and errors
-            contrast = "soft", -- can be "hard", "soft" or empty string
-            transparent_mode = true,
-        })
-        vim.cmd("colorscheme gruvbox")
+        vim.cmd("colorscheme gruvbox-material")
     end
 }
