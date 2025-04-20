fc6e6809d344b2205deda3419937d124430fed6f
Daniel Vieites <dvieitest@gmail.com>
2025-04-20 11:10:31 +0200

    lualine file name

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -14,7 +14,7 @@ return {
                     'branch',
                     icon = ''
                 }, 'diff' },
-            lualine_c = { 'filename' },
+            lualine_c = { { 'filename', path = 4 } },
             lualine_x = { 'filetype' },
             lualine_y = { 'progress' },
             lualine_z = { 'location' }
