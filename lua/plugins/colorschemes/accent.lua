bbf6c9fdca5c1662cef600cf37695860d0167427
Daniel Vieites <dvieitest@gmail.com>
2025-04-23 16:41:59 +0200

    update configs

--- lua/plugins/colorschemes/accent.lua
+++ lua/plugins/colorschemes/accent.lua
@@ -3,7 +3,7 @@ return {
     priority = 1000,
     'alligator/accent.vim',
     config = function()
-        -- vim.g.accent_colour="cyan" -- yellow (default)
+        vim.g.accent_colour="green" -- yellow (default)
         vim.g.accent_no_bg=1
         vim.g.accent_darken=1
         vim.cmd.colorscheme "accent"
--- lua/plugins/colorschemes/ashen.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "ficcdaf/ashen.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        require("ashen").load()
-        vim.cmd("colorscheme ashen")
-    end
-}
