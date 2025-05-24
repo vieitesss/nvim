a70d2e3cb1e4253af91ed9ca76229a725b1c5a8a
Daniel Vieites <dvieitest@gmail.com>
2025-05-24 16:46:03 +0200

    update some thigs

--- lua/vt/command.lua
+++ lua/vt/command.lua
@@ -3,7 +3,6 @@ return {
 	dev = true,
   -- "vieitesss/command.nvim",
 	cmd = { "CommandRexecute", "CommandExecute" },
-  config = true,
 	keys = {
 			{ "<space>co", ":CommandExecute<cr>" },
 			{ "<space>cr", ":CommandRexecute<cr>" },
