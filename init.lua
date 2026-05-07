d88b8c609dcc8ed64ce9a3e061cc2444aa885edc
Daniel Vieites <dvieitest@gmail.com>
2026-05-07 19:04:41 +0200

    style: formatting

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-require 'plugins'
+require("plugins")
 require("packui").setup()
-require 'configs'
-require 'lsp'
-require 'keymaps'
-require 'statusline'
-require 'autocmds'
-require 'umbraline'
+require("configs")
+require("lsp")
+require("keymaps")
+require("statusline")
+require("autocmds")
+require("umbraline")
