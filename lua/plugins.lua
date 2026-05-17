c6ba6f3cc39f74f027f4e719c22dcfcdad860024
Daniel Vieites <dvieitest@gmail.com>
2026-05-17 22:07:23 +0200

    feat: don't need gitsigns any more

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,9 +3,8 @@ vim.g.mapleader = " "
 vim.pack.add({
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/vieitesss/miniharp.nvim", version = "nightly" },
-    { src = vim.env.HOME .. "/personal/minifugit.nvim", version = "fix/enter-entry-unsaved-buffer" },
+    { src = vim.env.HOME .. "/personal/minifugit.nvim", version = "feat/file-change-counts" },
     { src = 'https://github.com/dmtrKovalenko/fff.nvim' },
-    { src = "https://github.com/lewis6991/gitsigns.nvim" },
     {
         src = "https://github.com/saghen/blink.cmp",
         version = vim.version.range("^1"),
@@ -59,7 +58,6 @@ require("mason").setup({})
 --         strings = false,
 --     },
 -- })
-require("gitsigns").setup({ signcolumn = false })
 require("blink.cmp").setup({
     fuzzy = { implementation = "prefer_rust_with_warning" },
     signature = { enabled = true },
@@ -132,17 +130,3 @@ require("oil").setup({
         show_hidden = true,
     },
 })
-
--- vim.g.vimtex_imaps_enabled = 0
--- vim.g.vimtex_view_method = "skim"
--- vim.g.latex_view_general_viewer = "skim"
--- vim.g.latex_view_general_options =
---     "-reuse-instance -forward-search @tex @line @pdf"
--- vim.g.vimtex_compiler_method = "latexmk"
--- vim.g.vimtex_quickfix_open_on_warning = 0
--- vim.g.vimtex_quickfix_ignore_filters = {
---     "Underfull",
---     "Overfull",
---     "LaTeX Warning: .\\+ float specifier changed to",
---     "Package hyperref Warning: Token not allowed in a PDF string",
--- }
