05e26168b2288fd8a24110d2b4fa035297359cc6
Daniel Vieites <dvieitest@gmail.com>
2026-05-25 20:25:04 +0200

    chore: unnecesary code

--- go/test-rpc/test.go
+++ go/test-rpc/test.go
@@ -7,12 +7,6 @@ import (
 	"github.com/neovim/go-client/nvim"
 )
 
-type Args struct {
-	A, B int
-}
-
-type Test struct{}
-
 func parseArgs(args []string) (int, int, error) {
 	if len(args) != 2 {
 		return 0, 0, fmt.Errorf("expected exactly two integers, got %d.", len(args))
