9a8c338da5e97ff3025eefb07279539769de558f
Daniel Vieites <dvieitest@gmail.com>
2026-05-25 20:29:31 +0200

    fix: validate channel

--- lua/features/test-rpc/init.lua
+++ lua/features/test-rpc/init.lua
@@ -4,16 +4,25 @@ local binary = vim.fn.stdpath("config") .. "/bin/test-rpc"
 
 local channel
 
+---@return number?
 local function rpc()
     if not channel then
         channel = vim.fn.jobstart(binary, { rpc = true })
+    end
+
+    if channel < 1 then
+        vim.log.error("an error ocurred starting the test-rpc job")
+        return nil
     end
 
     return channel
 end
 
 M.run = function()
-    local result = vim.rpcrequest(rpc(), "multiply", { "2", "a" })
+    local c = rpc()
+    if not c then return end
+
+    local result = vim.rpcrequest(c, "multiply", { "2", "a" })
     vim.print("Result: " ..  result)
 end
 
