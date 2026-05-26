1cd70199c7c32654b31f9a6c60e905f5402ff00d
Daniel Vieites <dvieitest@gmail.com>
2026-05-26 19:51:14 +0200

    feat: ensure_autocmd and on_ready function call

--- lua/features/test-rpc/init.lua
+++ lua/features/test-rpc/init.lua
@@ -3,26 +3,26 @@ local M = {}
 local binary = vim.fn.stdpath("config") .. "/bin/test-rpc"
 local socket = "/tmp/test-rpc.sock"
 
+local job
 local channel
 local id = 0
 local timeout = 50 -- ms
 
-local function on_data(_, data, _)
-    vim.print(data)
-    local info = vim.json.decode(data[1])
-    vim.print("result: " .. info.result)
-end
-
+-- Returns the channel id or nil
 ---@return number?
-local function try_connect()
+local function try_connect_socket()
     local times = 50
     local ok, chan
 
     while times > 0 do
-        ok, chan =
-            pcall(vim.fn.sockconnect, "pipe", socket, { on_data = on_data })
+        ok, chan = pcall(vim.fn.sockconnect, "pipe", socket, {
+            on_data = function(_, data, _)
+                vim.print(data)
+                local info = vim.json.decode(data[1])
+                vim.print("result: " .. info.result)
+            end,
+        })
         if ok and chan > 0 then
-            vim.print(50 - times)
             return chan
         end
 
@@ -33,45 +33,53 @@ local function try_connect()
     return nil
 end
 
----@return number?
-local function rpc()
-    if channel and channel > 0 then
-        return channel
-    end
-
-    local job = vim.fn.jobstart(binary, {
+---@return number
+local function start_job()
+    local j = vim.fn.jobstart(binary, {
         on_exit = function()
             channel = nil
             id = 0
-        end
+        end,
     })
-    if job == 0 then
+    if j == 0 then
         vim.notify(
             "invalid arguments to jobstart(): `" .. binary .. "` and `nil`",
             vim.log.levels.ERROR
         )
-        return nil
     end
-    if job == -1 then
+    if j == -1 then
         vim.notify(
             "binary `" .. binary .. "` is not executable",
             vim.log.levels.ERROR
         )
-        return nil
     end
 
-    if not channel then
-        channel = try_connect()
+    return j
+end
+
+---@param on_ready function
+local function rpc(on_ready)
+    if channel and channel > 0 then
+        on_ready()
+        return
     end
-    if not channel then
+
+    job = start_job()
+    if job < 1 then
+        return
+    end
+
+    if not channel or channel == 0 then
+        channel = try_connect_socket()
+    end
+    if channel == 0 then
         vim.notify(
             "an error occurred connecting to the socket " .. socket,
             vim.log.levels.ERROR
         )
-        return nil
     end
 
-    return channel
+    on_ready()
 end
 
 ---@param method string The server method
@@ -91,14 +99,25 @@ local function build_data(method, params)
     return data
 end
 
-M.multiply = function()
-    local c = rpc()
-    if not c then
-        return
-    end
+local function ensure_autocmd()
+    vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
+        pattern = { "*" },
+        callback = function(_)
+            if job and job > 0 then
+                vim.fn.jobstop(job)
+            end
+        end,
+    })
+end
 
-    local data = build_data("Multiply", { "2", "3" })
-    vim.api.nvim_chan_send(c, data)
+---@param a number
+---@param b number
+M.multiply = function(a, b)
+    ensure_autocmd()
+    rpc(function()
+        local data = build_data("Multiply", { tostring(a), tostring(b) })
+        vim.api.nvim_chan_send(channel, data)
+    end)
 end
 
 return M
