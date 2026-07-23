fab2293fada4017a1b326c6c35c3344d451b6f5d
Daniel Vieites <dvieitest@gmail.com>
2026-07-23 12:19:58 +0200

    feat: add some plugins and lsp

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- Configs
+vim.opt.signcolumn = 'yes'
 vim.opt.wildmenu = true
 vim.opt.wildmode = "longest:full,full"
 vim.opt.wildoptions = "pum"
@@ -25,17 +26,32 @@ require('vim._core.ui2').enable({
     msg = {
         targets = 'msg', -- 'cmd'|'msg'|'pager'
         msg = {
-      height = 0.5,
+            height = 0.5,
+        },
     },
-  },
 })
 
 -- Plugins
 vim.pack.add({
-    "https://github.com/blazkowolf/gruber-darker.nvim",
+    "https://github.com/mason-org/mason.nvim",
+    -- local: gruber-lighter (see rtp below)
     "https://github.com/stevearc/oil.nvim",
     "https://github.com/dmtrKovalenko/fff",
-    "https://github.com/vieitesss/minifugit.nvim"
+    "https://github.com/vieitesss/minifugit.nvim",
+    "https://github.com/vieitesss/miniharp.nvim",
+    "https://github.com/vieitesss/command.nvim"
+})
+
+require("mason").setup()
+
+local miniharp = require('miniharp')
+miniharp.setup({
+    notifications = false,
+    ui = {
+        position = 'top-right',
+        show_hints = false,
+        enter = false,
+    },
 })
 
 require("oil").setup({
@@ -51,16 +67,17 @@ require("oil").setup({
 })
 
 vim.g.fff = {
-  lazy_sync = true,
-  debug = { enabled = true, show_scores = true },
-  layout = {
-      width = 1,
-      height = 0.5,
-      anchor = "bottom"
-  }
+    lazy_sync = true,
+    debug = { enabled = true, show_scores = true },
+    layout = {
+        width = 1,
+        height = 0.5,
+        anchor = "bottom"
+    }
 }
 
-vim.cmd.colorscheme("gruber-darker")
+vim.opt.rtp:prepend(vim.fn.expand("~/personal/gruber-lighter.nvim"))
+vim.cmd.colorscheme("gruber-lighter")
 
 -- Mappings
 vim.g.mapleader = " "
@@ -68,23 +85,40 @@ vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { silent = true })
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { silent = true })
 vim.keymap.set("n", "<c-d>", "<c-d>zz", { silent = true })
 vim.keymap.set("n", "<c-u>", "<c-u>zz", { silent = true })
+vim.keymap.set("v", "<leader>y", '"*y', { desc = "Paste to the clipboard" })
+vim.keymap.set("n", "<leader>fo", function() vim.lsp.buf.format() end, { desc = "_FO_rmat using LSP" })
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { desc = "_G_o to _D_efinition" })
+-- -- fff
 vim.keymap.set('n', '<leader>ff', function() require('fff').find_files() end)
 vim.keymap.set('n', '<leader>fg', function() require('fff').live_grep() end)
-vim.keymap.set("v", "<leader>y", '"*y', { desc = "Paste to the clipboard" })
+-- -- oil
+vim.keymap.set("n", "<leader>of", "<cmd>Oil<cr>", { silent = true, desc = "_O_pen _F_ile directory" })
+vim.keymap.set("n", "<leader>oc", "<cmd>Oil " .. vim.fn.getcwd() .. "<cr>", { silent = true, desc = "_O_pen _C_WD" })
+-- -- minifugit
+vim.keymap.set("n", "<leader>gs", "<cmd>MinifugitStatus<cr>", { silent = true, desc = "_G_it _S_tatus" })
+-- -- miniharp
+vim.keymap.set('n', '<leader>m', miniharp.toggle_file, { desc = 'miniharp: toggle file mark' })
+vim.keymap.set('n', '<leader>l', miniharp.show_list, { desc = 'miniharp: toggle marks list' })
+vim.keymap.set('n', '<leader>L', miniharp.enter_list, { desc = 'miniharp: enter marks list' })
+vim.keymap.set('n', '<C-j>', function() miniharp.go_to(1) end, { desc = 'miniharp: go to mark 1' })
+vim.keymap.set('n', '<C-k>', function() miniharp.go_to(2) end, { desc = 'miniharp: go to mark 2' })
+vim.keymap.set('n', '<C-l>', function() miniharp.go_to(3) end, { desc = 'miniharp: go to mark 3' })
+-- -- command
+vim.keymap.set('n', '<leader>ce', '<Plug>(CommandExecute)')
+vim.keymap.set('n', '<leader>cl', '<Plug>(CommandExecuteLast)')
+vim.keymap.set('x', '<leader>ce', '<Plug>(CommandExecuteSelection)')
+vim.keymap.set('n', '<leader>cr', '<Plug>(CommandReopenTerminal)')
 
-vim.keymap.set("n", "<leader>of", "<cmd>Oil<cr>", { silent = true, desc = "_O_pen _F_ile directory" } )
-vim.keymap.set("n", "<leader>oc", "<cmd>Oil " .. vim.fn.getcwd() .. "<cr>", { silent = true, desc = "_O_pen _C_WD" } )
-vim.keymap.set("n", "<leader>gs", "<cmd>MinifugitStatus<cr>", { silent = true, desc = "_G_it _S_tatus" } )
 
 -- Autocmds
 vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if name == 'fff.nvim' and (kind == 'install' or kind == 'update') then
-      if not ev.data.active then vim.cmd.packadd('fff.nvim') end
-      require('fff.download').download_or_build_binary()
-    end
-  end,
+    callback = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if name == 'fff.nvim' and (kind == 'install' or kind == 'update') then
+            if not ev.data.active then vim.cmd.packadd('fff.nvim') end
+            require('fff.download').download_or_build_binary()
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -106,4 +140,18 @@ vim.api.nvim_create_user_command("Term", function(opts)
 end, {
     nargs = "*",
     complete = "shellcmd",
+})
+
+-- LSP
+local lsps = {
+    'rust-analyzer',
+    'lua_ls',
+}
+
+for _, l in ipairs(lsps) do
+    vim.lsp.enable(l)
+end
+
+vim.lsp.config('*', {
+    capabilities = vim.lsp.protocol.make_client_capabilities()
 })
